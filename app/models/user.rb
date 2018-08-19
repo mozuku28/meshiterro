f998255a34c281438f701bf78821d2572e8baf1b
野澤優輔 <nozawayuusuke@nozawayuusukenoMacBook.local>
2018-08-19 17:17:06 +0900

    lesson4 end

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :post_images
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'eaf87df617dd7e5fad3da43068ee00352b159396ee3704f88925e291ce43c33560d0e9bfc131f4581f91a0ceda71285ef6e2e2c2a3232c8b96d529841ff2df31'
